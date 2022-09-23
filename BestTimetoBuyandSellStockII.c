3d0cbf035fc206b662c95e3058bd7075d57b99d2
ram-a11y <59955586+ram-a11y@users.noreply.github.com>
2022-09-23 13:59:15 +0800

    Update BestTimetoBuyandSellStockII.c

--- BestTimetoBuyandSellStockII.c
+++ BestTimetoBuyandSellStockII.c
@@ -1,3 +1,14 @@
+#include<stdio.h>
+
+int maxProfit(const int *, int);
+
+int main(){
+    const int prices[] = {7,1,5,3,6,4};
+    const int pricesSize = 6;
+    printf("%d\n", maxProfit(prices, pricesSize));
+    return 0;
+}
+
 int maxProfit(int* prices, int pricesSize) {
   int sum = 0;
   for (int i = 1; i < pricesSize; i++) {
